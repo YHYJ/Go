418e445d41f963c68325db02ebb82c18dcbf4782
YJ1516 <yj1516268@outlook.com>
2018-01-07 09:02:56 +0800

    1-7

--- src/github.com/YHYJ/hello/main.go
+++ src/github.com/YHYJ/hello/main.go
@@ -3,5 +3,7 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hello World")
+	fmt.Println("Name: YJ1516")
+	fmt.Println("Site: http://yj1516.site/")
+	fmt.Println("Mail: yj1516268@gmail.com")
 }
