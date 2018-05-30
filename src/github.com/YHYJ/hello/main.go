f7c888b58595a763ebcb3e94ca9f3eb92307a6b8
YHYJ <yj1516268@outlook.com>
2018-05-30 22:28:09 +0800

    05-30

--- src/github.com/YHYJ/hello/main.go
+++ src/github.com/YHYJ/hello/main.go
@@ -2,8 +2,19 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Name: YJ1516")
-	fmt.Println("Site: http://yj1516.site/")
-	fmt.Println("Mail: yj1516268@gmail.com")
+func main() {
+	fmt.Println("---------------------------------------<-个人信息")
+	fmt.Println("| 姓名: 王斌")
+	fmt.Println("| 性别: 男")
+	fmt.Println("| 年龄: 1996-11-01")
+	fmt.Println("| 籍贯: 山东省青岛市")
+	fmt.Println("---------------------------------------<-社交信息")
+	fmt.Println("| Nums: 18765426009	19919941516")
+	fmt.Println("| QQ  : 1339670004")
+	fmt.Println("| WeChat    : yh1339670004")
+	fmt.Println("| Work-Mail : yj1516268@outlook.com")
+	fmt.Println("| Daily-Mail: yj1516268@gmail.com")
+	fmt.Println("---------------------------------------<-其他信息")
+	fmt.Println("| MySite: http://yj1516.site/")
+	fmt.Println("| github: https://github.com/YHYJ/")
 }
