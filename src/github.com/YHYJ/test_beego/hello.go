f0275cd42c9cac5dc46796788bb458729938db92
YJ <YJ1516268@outlook.com>
2020-01-13 14:16:51 +0800

    test beego

---
+++ src/github.com/YHYJ/test_beego/hello.go
@@ -0,0 +1,16 @@
+/* File: hello.go */
+/* Auther: YJ */
+/* Email: yj1516268@outlook.com */
+/* Created Time: 2020-01-13 14:15:47 */
+
+// Description:
+
+package main
+
+import (
+	"github.com/astaxie/beego"
+)
+
+func main() {
+    beego.Run()
+}
